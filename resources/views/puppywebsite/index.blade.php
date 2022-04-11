d8e7292f41ec6266cfb12b0cd28e340ff2d43dd0
Hoang <hoanghip108@gmail.com>
2022-04-11 21:04:22 +0700

    css admin

--- resources/views/puppywebsite/index.blade.php
+++ resources/views/puppywebsite/index.blade.php
@@ -1,32 +1,58 @@
 @extends('puppywebsite.PuppyWebsiteMaster.PuppyMaster')
 
 @section('main')
-    <table width="100%" border="1">
-        <tr>
+    <div class="container-fluid">
+        <h1>PECA managements</h1>
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="left">
+                    <div class="card">
+                        <div class="card-header">
+                            Quicklink
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">
+                                <a class="quick-link" href="">Home Page</a>
+                            </li>
+                            <li class="list-group-item">
+                                <a class="quick-link" href="">Log out</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-            <th>ID</th>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Area</th>
-            <th>Breeds</th>
-            <th>Detail</th>
-            <th>Image</th>
-            <th>Color</th>
-        </tr>
-        <tr>
-            @foreach($result as $each):
-                <td>{{$each->id}}</td>
-                <td>{{$each->name}}</td>
-                <td>{{$each->gender}}</td>
-                <td>{{$each->area}}</td>
-                <td>{{$each->breedsID}}</td>
-                <td>{{$each->Detail}}</td>
-                <td>
-                    <img src="{{$each->image}}" alt="">
+            <div class="col-lg-9">
+                <div class="right">
+                    <table class="table">
+                        <thead class="thead-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Gender</th>
+                            <th>Area</th>
+                            <th>Breeds</th>
+                            <th>Detail</th>
+                            <th>Image</th>
+                            <th>Color</th>
+                        </tr>
+                        <tr>
+                            @foreach($result as $each):
+                            <td>{{$each->id}}</td>
+                            <td>{{$each->name}}</td>
+                            <td>{{$each->gender}}</td>
+                            <td>{{$each->area}}</td>
+                            <td>{{$each->breedsID}}</td>
+                            <td>{{$each->Detail}}</td>
+                            <td>
+                                <img src="{{$each->image}}" alt="">
+                            </td>
+                            <td>{{$each->color}}</td>
+                            @endforeach
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
 
-                </td>
-                <td>{{$each->color}}</td>
-            @endforeach
-        </tr>
-    </table>
 @endsection
